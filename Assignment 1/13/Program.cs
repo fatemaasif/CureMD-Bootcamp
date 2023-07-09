d786170ce9818fedc7ced63216ccdf47b3b280de
fatemaasif <fatemaasif268@gmail.com>
2023-07-09 20:36:42 +0500

    Q13

---
+++ Assignment 1/13/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace _13
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter the height of the right angle triangle: ");
+            int height = int.Parse(Console.ReadLine());
+            for (int i = 0; i <= height; i++)
+            {
+                for (int j = 0; j < i; j++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+            }
+
+        }
+    }
+}
