626b7033aef98e6c2f50fdf316a0a3a17849d746
fatemaasif <fatemaasif268@gmail.com>
2023-07-09 20:26:09 +0500

    Q12

--- Assignment 1/12/Program.cs
+++ Assignment 1/12/Program.cs
@@ -4,7 +4,16 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.Write("Enter a word or phrase to check if it's a palindrome: ");
+            string text = Console.ReadLine();
+            text = text.Replace(" ", ""); //remove spaces
+            string reversed = "";
+            for (int i = text.Length-1; i >= 0; i--)
+            {
+                reversed = reversed + text[i];
+            }
+            string result = reversed == text ? "It is a palindrome!" : "It is not a palindrome!";
+            Console.WriteLine(result);
         }
     }
 }
