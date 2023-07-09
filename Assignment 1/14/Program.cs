63e1370f6b90190ebb9eb65156dbf219697b8668
fatemaasif <fatemaasif268@gmail.com>
2023-07-09 20:53:23 +0500

    q13 and q14 are swapped

---
+++ Assignment 1/14/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace _14
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a list of numbers seperated by a space: ");
+            string[] numbers_str = Console.ReadLine().Split(' '); //splits the numbers into an array of strings
+            int[] numbers = Array.ConvertAll(numbers_str, int.Parse); //converts string array into int array
+            int largest = numbers[0];
+            int smallest = numbers[0];
+            foreach (int i in numbers)
+            {
+                if (i > largest)
+                { 
+                    largest = i; 
+                }
+                if (i < smallest)
+                {
+                    smallest = i;
+                }
+            }
+            
+            Console.WriteLine("Largest number = " + largest);
+            Console.WriteLine("Smallest number = " + smallest);
+        }
+    }
+}
