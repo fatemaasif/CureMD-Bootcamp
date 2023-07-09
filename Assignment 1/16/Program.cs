4fb5344f994b9ce4fd0de363e618ff02d46689fe
fatemaasif <fatemaasif268@gmail.com>
2023-07-09 21:44:23 +0500

    q16

---
+++ Assignment 1/16/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace _16
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter string to be reversed");
+            string text = Console.ReadLine();
+            string reversed="";
+            for (int i = text.Length-1; i >= 0; i--)
+            {
+                reversed = reversed + text[i];
+            }
+            Console.WriteLine("The reversed string is: " + reversed);
+        }
+    }
+}
