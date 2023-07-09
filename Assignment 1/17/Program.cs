c7b425297973d820c13fa935345fca046954138f
fatemaasif <fatemaasif268@gmail.com>
2023-07-09 23:36:33 +0500

    Q17

---
+++ Assignment 1/17/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace _17
+{
+    internal class Program
+    { //Write a program that sorts an array. (take input as random numbers)
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a list of numbers to sort them: ");
+            string input = Console.ReadLine();
+            string[] numbers = input.Split(' ');
+            int[] array = Array.ConvertAll(numbers, int.Parse); //converts string array to num array
+            int temp;
+            for (int i = 0; i < array.Length; i++)
+            {
+                for (int j = i+1; j <array.Length ; j++)
+                {
+                    if (array[i] > array[j])
+                    {
+                        temp = array[i];
+                        array[i] = array[j];
+                        array[j] = temp;
+                    }
+                }
+            }
+            Console.WriteLine("The sorted array is: ");
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.Write(array[i]+" ");
+            }
+
+        }
+    }
+}
