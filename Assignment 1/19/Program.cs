d39071ac4e369137331cd683a37eb28cea2d967a
fatemaasif <fatemaasif268@gmail.com>
2023-07-10 00:13:32 +0500

    Q19

---
+++ Assignment 1/19/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace _19
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter number of rows: ");
+            int rows = int.Parse(Console.ReadLine());
+            for (int i = 1; i <= rows; i++)
+            {
+                for (int j = 1; j <= i; j++)
+                {
+                    Console.Write(j);
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
