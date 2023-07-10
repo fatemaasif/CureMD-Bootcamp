1bd8ec4f26cd1c0dfcab4310185c611ca7785497
fatemaasif <fatemaasif268@gmail.com>
2023-07-11 01:41:04 +0500

    assignment task 1 created

---
+++ Assignment 2/Task 1/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace Task_1
+{
+    class Animal()
+    {
+        public string name, age, species;
+        public Eat() { Console.WriteLine("The animal is eating"); }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
