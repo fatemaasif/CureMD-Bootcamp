20cc117a2e7f9e370ee330b1b9259fe8c17a3ab8
fatemaasif <74526981+fatemaasif@users.noreply.github.com>
2023-07-10 03:29:25 -0400

    Add files via upload

---
+++ Project 1/Project 1/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a sentence: ");
+            string input = Console.ReadLine();
+            string[] text = input.Split(' ');
+            int freq;
+            int[] frequencyarr = new int[10];
+            string[] temparr=new string[10];
+            for (int i = 0; i < text.Length; i++) //takes each word
+            {
+                freq=0;
+                for (int j = i + 1; j < text.Length; j++) //compares to all words
+                {
+                    if (text[i]==text[j])
+                    {
+                        freq++;
+                    }
+                }
+                frequencyarr[i]= freq;
+            }
+            bool flag=false; //flags if word already quoted before
+            for (int i = 0; i < text.Length; i++) //takes each word and prints its frequency
+            {
+                if (frequencyarr[i]<=1)
+                {
+                    Console.WriteLine(text[i] + ": " + frequencyarr[i]);
+                }
+                else
+                {
+                    if(!flag)
+                    {
+                        Console.WriteLine(text[i] + ": " + frequencyarr[i]);
+                        flag=true;
+                    }
+                }
+            }
+            Console.ReadKey();
+            
+        }
+    }
+}
