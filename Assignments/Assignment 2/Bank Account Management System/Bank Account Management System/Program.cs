87ca6cbdc147b02a86e863c3ca33ef2a220c959e
fatemaasif <fatemaasif268@gmail.com>
2023-07-13 03:27:33 -0400

    v3 updated ith comments to assgn2

--- Assignments/Assignment 2/Bank Account Management System/Bank Account Management System/Program.cs
+++ Assignments/Assignment 2/Bank Account Management System/Bank Account Management System/Program.cs
@@ -1,23 +1,33 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+
 
 namespace Bank_Account_Management_System
 {
     public interface IBankAccount
+        /// <summary> This is an interface with functions that all accounts must have in order to fit the characteristics of a bank account
     {
         void Deposit(int amount);
         void Withdraw(int amount);
     }
-    public abstract class BankAccount:IBankAccount
+    public interface ITransaction //This is an interface with functions that all accounts must have in order to fit the characteristics of a bank account
     {
-        public int AccountNumber {get; set;}
-        public double Balance {get; set;}
-        public string AccHolName {get; set;}
-        public double interestrate = 0.05;
-        public BankAccount(int accnum, string holname, int bal)
+        void ExecuteTransaction(decimal amount, string transctiontype);
+        void PrintTransaction(decimal transactionamount, string transctiontype);
+    }
+    public abstract class BankAccount:IBankAccount,ITransaction 
+    ///<summary>
+    ///This is an example of:
+    ///1. Abstraction where the internal details of the class are hidden and the concept of a general bank account are abstracted.
+    ///2. Realization/Implementation: the BankAccount class implements the two different interfaces.
+    ///</summary>
+    {
+        // These getters and setters are examples of encapsulation where the attributes themselves are hidden but their return value is made availabe through getters and setters
+        protected int AccountNumber {get; set;}
+        protected double Balance {get; set;}
+        protected string AccHolName {get; set;}
+        private double interestrate = 0.05; //5% default interest applied
+        public BankAccount(int accnum, string holname, double bal)
         {
             AccountNumber = accnum;
             AccHolName = holname;
@@ -25,31 +35,19 @@ namespace Bank_Account_Management_System
         }
         public virtual void Deposit(int depositamount)
         {
-            Balance += depositamount;
-            Console.WriteLine("$"+depositamount+" has been deposited into your account");
-            Console.WriteLine("Your current balance is $" + Balance);
-            Console.WriteLine();
+            ExecuteTransaction(depositamount, "Deposit");
         }
-        public virtual void Deposit(double depositamount) //overloaded deposit method with double arg
+        public virtual void Deposit(double depositamount) //Example of static polymorphism: The deposit method is overloaded with a double type arg
         {
-            Balance += depositamount;
-            Console.WriteLine("$"+depositamount+" has been deposited into your account");
-            Console.WriteLine("Your current balance is $" + Balance);
-            Console.WriteLine();
+            ExecuteTransaction(Convert.ToDecimal(depositamount), "Deposit");
         }
         public virtual void Withdraw(int withdrawamount)
         {
-            Balance -= withdrawamount;
-            Console.WriteLine("$" + withdrawamount + " has been debited from your account");
-            Console.WriteLine("Your current balance is $" + Balance);
-            Console.WriteLine();
+            ExecuteTransaction(withdrawamount, "Withdraw");
         }
-        public virtual void Withdraw(double withdrawamount) //overloaded withdraw method with double arg
+        public virtual void Withdraw(double withdrawamount) //Example of static polymorphism: The deposit method is overloaded with a double type arg
         {
-            Balance -= withdrawamount;
-            Console.WriteLine("$" + withdrawamount + " has been debited from your account");
-            Console.WriteLine("Your current balance is $" + Balance);
-            Console.WriteLine();
+            ExecuteTransaction(Convert.ToDecimal(withdrawamount), "Withdraw");
         }
         public void DisplayAccountInfo()
         {
@@ -59,17 +57,30 @@ namespace Bank_Account_Management_System
             Console.WriteLine("Total Balance: " + Balance);
             Console.WriteLine();
         }
-        public virtual double CalculateInterest()
+        public virtual double CalculateInterest(double amount)
         {
-            //private double interestRate = 0.05;
-            //private double timeSinceBalanceUpdated = 1;
-            private double TotalInterest = 1;//Balance*interestRate*timeSinceBalanceUpdated;
-            return TotalInterest;
+            return amount * interestrate; 
+        } 
+        public void ExecuteTransaction(decimal transactionamount, string transctiontype)
+        {
+            if(transctiontype== "Deposit") { Balance += Convert.ToDouble(transactionamount); }
+            if (transctiontype == "Withdraw") { Balance -= Convert.ToDouble(transactionamount); }
+            PrintTransaction(transactionamount, transctiontype);
         }
-    }
+        public void PrintTransaction(decimal transactionamount, string transctiontype)
+        {
+            if (transctiontype == "Deposit") { Console.WriteLine("$" + transactionamount + " has been deposited into your account"); }
+            if (transctiontype == "Withdraw") { Console.WriteLine("$" + transactionamount + " has been withdrawn from your account"); }
+            Console.WriteLine("Your current balance is $" + Balance);
+            Console.WriteLine();
+        }
+    } 
+    /// <summary>
+    /// This is an example of inheritance: The classes that follow (up till but excluding the class Bank) will inherit from the Parent class BankAccount.
+    /// </summary>
     public class SavingsAccount : BankAccount
     {
-        public SavingsAccount(int anum, string hn, int bal) : base(anum, hn, bal) 
+        public SavingsAccount(int anum, string hn, double bal) : base(anum, hn, bal) 
         {
             Console.WriteLine("You have succesfully created a Savings Account with us. Thank you!");
             Console.WriteLine();
@@ -77,7 +88,7 @@ namespace Bank_Account_Management_System
         
         public override void Deposit(int depositamount)
         {
-            double modifieddeposit = depositamount*(1+interestRate);
+            double modifieddeposit = CalculateInterest(depositamount);
             Balance += modifieddeposit;
             Console.WriteLine("$" + modifieddeposit + " has been deposited into your account against a deposit of $"+depositamount);
             Console.WriteLine("Your current balance is $" + Balance);
@@ -85,14 +96,16 @@ namespace Bank_Account_Management_System
         }
         public override void Deposit(double depositamount)
         {
-            double modifieddeposit = depositamount*(1+interestRate);
+            double modifieddeposit = CalculateInterest(depositamount);
             Balance += modifieddeposit;
             Console.WriteLine("$" + modifieddeposit + " has been deposited into your account against a deposit of $"+depositamount);
             Console.WriteLine("Your current balance is $" + Balance);
             Console.WriteLine();
         }
-        public override double CalculateInterest()
-        {}
+        public override double CalculateInterest(double amount) //Example of dynamic polymorphism: The calculateinterest method is overridden to fit the requirements of the savingsaccount class
+        {
+            return amount * 1.2; //20% interest applied in savings account
+        }
     }
     public class CheckingAccount : BankAccount
     {
@@ -113,8 +126,10 @@ namespace Bank_Account_Management_System
                 base.Withdraw(withdrawamount);
             }
         }
-        public override double CalculateInterest()
-        {}
+        public override double CalculateInterest(double amount) //Example of dynamic polymorphism: The calculateinterest method is overridden to fit the requirements of the checkingaccount class
+        {
+            return amount * 0.10; //10% interest applied
+        }
     }
     public class LoanAccount : BankAccount
     {
@@ -123,10 +138,19 @@ namespace Bank_Account_Management_System
             Console.WriteLine("You have succesfully created a Loan Account with us. Thank you!");
             Console.WriteLine();
         }
-        public override double CalculateInterest()
-        {}
+        public override double CalculateInterest(double amount) //Example of dynamic polymorphism: The calculateinterest method is overridden to fit the requirements of the loanaccount class
+        {
+            return amount * 0.20; //20% interest applied in loan account
+        }
     }
     public class Bank
+    /// <summary>
+    /// The Bank class is an example of:
+    /// 1. Association: It "has-a" bank account
+    /// 2. Aggregation: It is a collection of bank account of whom the bank is the owner
+    /// 3. Dependency: The bank account objects need a bank object to be created
+    /// </summary>
+
     {
         public List<BankAccount> ListofAccounts { get; set; }
         public Bank()
