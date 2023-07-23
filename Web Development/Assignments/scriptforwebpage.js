b9a6940fecf179dedc854a21683e8a46fb0778c5
fatemaasif <fatemaasif268@gmail.com>
2023-07-24 00:34:38 +0500

    updatedassignment

---
+++ Web Development/Assignments/scriptforwebpage.js
@@ -0,0 +1,16 @@
+const linksea = document.getElementById('SEA');
+linksea.textContent="New Region";
+/*
+const navbarlinks = document.querySelectorAll('.iteminnavbar');
+function changeColorOnHover(item) {
+    item.setAttribute('style','background-color:red');
+  }
+
+  function resetColorOnMouseOut(item) {
+    item.target.style.color = 'blue';
+  }
+
+  navbarlinks.forEach(link => {
+    link.addEventListener(onmouseover, changeColorOnHover);
+    link.addEventListener(onmouseout, resetColorOnMouseOut);
+  });*/
